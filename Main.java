944fee97914e0136b97320d44fd284fb9ab974fc
Zafer Tan ÇANKIRI <ztan.cankiri@gmail.com>
2020-07-30 18:34:17 +0300

    EOF is fixed.

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +11,9 @@ public class Main {
 
             String[] subtitles = new File(rootPath).list((dir, name) -> name.endsWith(".srt"));
             String outPath = Paths.get(rootPath, "out").toString();
-            new File (outPath).mkdir();
+            final boolean createdFolder = new File(outPath).mkdir();
 
-            for (String subtitle : subtitles) {
+            for (String subtitle : Objects.requireNonNull(subtitles)) {
                 String oldSubtitlePath = Paths.get(rootPath,  subtitle).toString();
                 String newSubtitlePath = Paths.get(rootPath, "out", subtitle).toString();
 
@@ -23,9 +24,11 @@ public class Main {
                 writer.write('\uFEFF');
                 char[] buffer = new char[1024];
                 int read;
+
                 while ((read = reader.read(buffer)) != -1) {
                     writer.write(buffer, 0, read);
                 }
+                writer.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
